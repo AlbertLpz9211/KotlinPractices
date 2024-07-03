e00648e65b70dbfb43c14324201deeba9cf0a9a6
Alberto Lopez <alberto.lopez_g@outlook.com>
2024-07-03 01:05:16 -0700

    first commit

---
+++ app/src/main/java/com/example/applicacionestudio/Nulabilidad.kt
@@ -0,0 +1,12 @@
+package com.example.applicacionestudio
+
+fun main() {
+    var  name:String? = null
+
+    println(name?.get(3) ?: "no digas mamadas, es nulo")
+
+
+
+    if(name=="Betho"){
+    }
+}
